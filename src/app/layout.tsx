b1e008833cc3395d29255789f9031084f185954e
Nayan-jai <turnipnayan000@gmail.com>
2026-07-12 22:45:48 +0530

    fix: self-host Font Awesome, remove CDN link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        />
+
       </head>
       <body className={`${inter.className} h-full relative`} suppressHydrationWarning>
         <Providers>
